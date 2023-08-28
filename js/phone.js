a5ee546499ef3a2a0ec290f8a9fb917a1e48639d
iamapusaha <apusaha392@gmail.com>
2023-08-28 21:18:07 +0600

    data loaded

--- js/phone.js
+++ js/phone.js
@@ -1,1 +1,6 @@
-console.log(connected);
+const loadPhone = async () => {
+    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+    const data = await res.json();
+    console.log(data.data[0]);
+}
+const data = loadPhone();
