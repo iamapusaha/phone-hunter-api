be09a4552265ada7ae6513d6e45820c6ea4b2f93
iamapusaha <apusaha392@gmail.com>
2023-08-28 22:43:02 +0600

    card design done with api data

--- js/phone.js
+++ js/phone.js
@@ -1,6 +1,29 @@
 const loadPhone = async () => {
     const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
     const data = await res.json();
-    console.log(data.data[0]);
+    displayPhones(data.data);
 }
-const data = loadPhone();
+const displayPhones = (phones) => {
+    const cardContainer = document.getElementById('card-container');
+    phones.forEach(phone => {
+        console.log(phone);
+        const phoneCard = document.createElement('div');
+        phoneCard.classList = `card min-w-96 bg-base-100 shadow-xl`;
+        phoneCard.innerHTML = `
+        <figure class="px-10 pt-10">
+                        <img src="${phone.image}" class="rounded-xl" />
+                    </figure>
+                    <div class="card-body items-center text-center">
+                        <h2 class="card-title">${phone.brand}</h2>
+                        <p>${phone.phone_name}</p>
+                        <div class="card-actions">
+                            <button class="btn btn-primary">Buy Now</button>
+                        </div>
+                    </div>
+        `
+        cardContainer.appendChild(phoneCard)
+    });
+
+}
+loadPhone();
+
